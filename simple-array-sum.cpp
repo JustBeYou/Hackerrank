813d1d1149dc4cc8a9302c65510052d1f8ab889e
JustBeYou <mihailferaru2000@gmail.com>
2018-07-14 10:41:02 +0300

    Add solution for simple-array-sum

---
+++ simple-array-sum.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int arr_i = 0;arr_i < n;arr_i++){
+       cin >> arr[arr_i];
+    }
+    
+    int s = 0;
+    for (int i = 0; i < n; i++) {
+        s += arr[i];
+    }
+    cout << s << endl;
+    
+    return 0;
+}
