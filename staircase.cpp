7fae4ed33bd66c87b0727edaac215d80fe0df11a
JustBeYou <mihailferaru2000@gmail.com>
2018-07-14 10:41:02 +0300

    Add solution for staircase

---
+++ staircase.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < n - i; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j < i; j++) {
+            cout << "#";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
