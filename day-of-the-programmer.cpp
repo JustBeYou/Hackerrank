6486a0c26995c4a199eb390b9c61ca4875a29c06
JustBeYou <mihailferaru2000@gmail.com>
2018-07-14 10:41:02 +0300

    Add solution for day-of-the-programmer

---
+++ day-of-the-programmer.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int mon7 = 215; //7 months' days
+
+string solve(int year)
+{
+    short int feb; //days in february
+    if(year < 1918) //julian
+        feb = year%4 ? 28 : 29;
+    else if(year > 1918) //gregorian
+        feb = !(year%400) || year%100&&!(year%4) ? 29 : 28;
+    else //dreaded 1918
+        feb = 15;
+    feb = 256 - (feb + mon7);
+    return to_string(feb) + ".09." + to_string(year);
+}
+
+int main() {
+    int year;
+    cin >> year;
+    string result = solve(year);
+    cout << result << endl;
+    return 0;
+}
